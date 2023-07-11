fba66720cb6ab87f04028682f1ab17b821f82f05
ev0clu <ev0clu@gmail.com>
2023-07-11 13:57:18 +0200

    Signup and Login routes are added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const index_controller = require('../controllers/indexController');
+const form_controller = require('../controllers/formController');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Members Only' });
-});
+router.get('/', index_controller.index_get);
+
+/* GET request for  up page. */
+router.get('/signup-form', form_controller.signup_get);
+
+router.post('/signup-form', form_controller.signup_post);
+
+router.get('/login-form', form_controller.login_get);
+
+router.post('/login-form', form_controller.login_post);
 
 module.exports = router;
