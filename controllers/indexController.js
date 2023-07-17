3dad399e1ba0faf997a64e26f2f0666161e31cde
ev0clu <ev0clu@gmail.com>
2023-07-17 12:34:25 +0200

    Content for index page is added

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,8 +1,9 @@
-const User = require('../models/User');
+const Message = require('../models/Message');
 const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require('express-validator');
 
 // Display Index page on GET.
 exports.index_get = asyncHandler(async (req, res, next) => {
-    res.render('index', { title: 'Members Only' });
+    const allMessages = await Message.find({}).sort({ createdAt: 1 }).populate('createdBy').exec();
+    res.render('index', { title: 'Members Only', allMessages: allMessages });
 });
