20ed524cf50e1395f6e332f2f048ea2e4da231d7
ev0clu <ev0clu@gmail.com>
2023-07-17 12:33:42 +0200

    Comments are added

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,16 @@ const form_controller = require('../controllers/formController');
 /* GET home page. */
 router.get('/', index_controller.index_get);
 
-/* GET request for  up page. */
+/* GET request for signup form page. */
 router.get('/signup-form', form_controller.signup_get);
 
+/* POST request for signup form page. */
 router.post('/signup-form', form_controller.signup_post);
 
+/* GET request for login form page. */
 router.get('/login-form', form_controller.login_get);
 
+/* POST request for login form page. */
 router.post('/login-form', form_controller.login_post);
 
 module.exports = router;
