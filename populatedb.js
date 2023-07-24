d9c03cff73c019502475ae2a06ca215cd0b59ea2
ev0clu <ev0clu@gmail.com>
2023-07-24 14:32:20 +0200

    Console.log content is updated for script

--- populatedb.js
+++ populatedb.js
@@ -1,7 +1,7 @@
 // #! /usr/bin/env node
 
 console.log(
-    'This script populates some test Movies, Directors, Genres and MovieInstances to your database. Specified database as argument - e.g.: node populatedb "mongodb+srv://cooluser:coolpassword@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority"'
+    'This script populates some test Messages and Users to your database. Specified database as argument - e.g.: node populatedb "mongodb+srv://cooluser:coolpassword@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority"'
 );
 
 // Get arguments passed on command line
